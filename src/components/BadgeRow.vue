3a841cd1953c06a8274618affd5f993d219b85d4
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 15:29:02 +0900

    :heavy_plus_sign:  add badge row component

---
+++ src/components/BadgeRow.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="badge-row" :class="{ 'badge-row-sidescroll': sidescroll }">
+    <badge v-for="(badge, index) in badges" :key="index" :badge="badge"/>
+  </div>
+</template>
+
+<script>
+import Badge from './Badge/index.js'
+
+export default {
+  props: {
+    badges: {
+      type: Array,
+      default: []
+    },
+    sidescroll: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    Badge
+  }
+}
+</script>
+
+<style lang="scss">
+.badge-row {
+  display: flex;
+  flex-direction: row;
+  max-width: 100%;
+  flex-wrap: wrap;
+}
+
+.badge-row-sidescroll {
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+}
+</style>
