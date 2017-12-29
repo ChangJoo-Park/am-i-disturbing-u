bde2e9972fd4b114601ad02c6bd23bc1a3279a7f
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 23:53:56 +0900

    :heavy_minus_sign: add member row scaffold

---
+++ src/components/MemberRow.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="member-row">
+    <div class="member-avatar-wrapper">
+      <avatar />
+    </div>
+    <div class="member-info-wrapper">
+      <div class="member-info">
+        <div>ChangJoo Park</div>
+        <div>some title</div>
+      </div>
+      <badge-row :badges="badges" />
+    </div>
+    <div class="member-row-action">
+      <button>Ping</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar'
+import BadgeRow from '@/components/BadgeRow'
+
+export default {
+  components: {
+    Avatar,
+    BadgeRow
+  },
+  data: function () {
+    return {
+      badges: [
+        {
+          body: 'Hello World',
+          type: 'text'
+        },
+        {
+          body: 'Hello World',
+          type: 'text'
+        },
+        {
+          body: 'Hello World',
+          type: 'text'
+        },
+        {
+          body: 'Hello World',
+          type: 'text'
+        },
+        {
+          body: 'Hello World',
+          type: 'text'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.member-row {
+  border-bottom: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 5px 10px;
+}
+.member-avatar-wrapper {
+  flex: 0;
+  margin-right: 10px;
+}
+.member-info-wrapper {
+  flex: 1;
+  margin-right: 10px;
+}
+.member-row-action {
+  flex: 0;
+}
+</style>
