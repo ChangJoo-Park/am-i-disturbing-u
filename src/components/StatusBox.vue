fa9b473166ca92496268a5ceff9ebc60f66d5e8f
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 23:02:38 +0900

    :heavy_plus_sign:  add status box

---
+++ src/components/StatusBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <form-box title='Status'>
+    <ul class="status-list">
+      <li class="status-item">
+        <span class="status-item-label">Next In</span>
+        <div>O</div>
+      </li>
+      <li class="status-item">
+        <span class="status-item-label">Next Out</span>
+        <div>O</div>
+      </li>
+      <li class="status-item">
+        <span class="status-item-label">Remote</span>
+        <div>O</div>
+      </li>
+      <li class="status-item">
+        <span class="status-item-label">Do not disturb</span>
+        <div>O</div>
+      </li>
+    </ul>
+  </form-box>
+</template>
+
+<script>
+import FormBoxLayout from '../layouts/FormBox'
+import BadgeRow from './BadgeRow.vue'
+
+export default {
+  components: {
+    'form-box': FormBoxLayout,
+  }
+}
+</script>
+
+<style lang="scss">
+.status-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  .status-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 5px 10px;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
