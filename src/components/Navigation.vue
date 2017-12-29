c2290df28673e694d77f6d8dba4f57bb5c0be2da
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 22:40:38 +0900

    :heavy_plus_sign:  add navigation scaffold

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <div class="navigation">
+    <div class="logo-wrapper">
+      <img src="https://www.google.co.kr/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" class="logo" width="100px">
+    </div>
+    <div class="navigation-actions-wrapper">
+      <div class="navigation-action-search">
+        <input type="text" class="navigation-action-search-input" placeholder="Search Team member">
+      </div>
+      <ul class="navigation-actions">
+        <li class="navigation-action"><button>Action One</button></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +20,49 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.navigation {
+  width: 100%;
+  height: 40px;
+  background-color: #ddd;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.logo-wrapper {
+  width: 100px;
+  height: 40px;
+  overflow: hidden;
+  display: flex;
+  margin-left: 20px;
+  align-content: center;
+  align-items: center;
 
+  .logo {
+    flex: 1;
+    width: 100px;
+    height: 30px;
+  }
+}
+
+.navigation-actions-wrapper {
+  margin-right: 20px;
+  display: flex;
+  align-content: center;
+  align-items: center;
+}
+
+.navigation-action-search {
+  margin-right: 5px;
+}
+
+.navigation-actions {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navigation-action {
+
+}
 </style>
