5aa22da1dcf46f7b8d268c92c8521f7ab9fac5d0
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-30 01:31:37 +0900

    :construction: working in progress Admin page

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,15 +1,49 @@
 <template>
   <div id="admin-page">
     <h1>This is Admin Page!</h1>
+    <div class="data-wrapper">
+      {{ teams }}
+    </div>
+    <div class="data-wrapper">
+      {{ users }}
+    </div>
+    <div class="data-wrapper">
+      {{ badges }}
+    </div>
+    <div class="data-wrapper">
+      {{ pings }}
+    </div>
+    <div class="data-wrapper">
+      {{ logs }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { teamsRef, usersRef, badgesRef, pingsRef, logsRef } from '../store'
 
+export default {
+  firebase: {
+    teams: teamsRef,
+    users: usersRef,
+    badges: badgesRef,
+    pings: pingsRef,
+    logs: logsRef
+  }
 }
 </script>
 
 <style>
-
+#admin-page {
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+.data-wrapper {
+  border: 1px solid black;
+  min-height: 400px;
+  overflow: hidden;
+  margin-bottom: 10px;
+}
 </style>
