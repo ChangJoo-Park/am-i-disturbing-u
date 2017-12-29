f629c43c11515306a0986cdf1fb7ff371bb29f2e
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 11:27:50 +0900

    :heavy_plus_sign: add avatar component and test

---
+++ src/components/Avatar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="avatar" :class="avatarClass">
+    <img :src="img" class="avatar-image">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: false,
+      default: 'http://via.placeholder.com/100x100'
+    },
+    size: {
+      type: String,
+      required: false,
+      default: 'normal',
+      validator: (value) => {
+        const SIZES = ['normal', 'large', 'small']
+        return SIZES.includes(value)
+      }
+    }
+  },
+  computed: {
+    avatarClass: function () {
+      return `avatar-${this.size}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.avatar {
+  display: inline-block;
+  border-radius: 100%;
+  overflow: hidden;
+  .avatar-image {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.avatar-normal {
+  width: 50px;
+  height: 50px;
+}
+
+.avatar-large {
+  width: 100px;
+  height: 100px;
+}
+
+.avatar-small {
+  width: 30px;
+  height: 30px;
+}
+</style>
