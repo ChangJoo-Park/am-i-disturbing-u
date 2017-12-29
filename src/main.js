f860ebe9cb4ce279a353aa0fbecbd1772c02a1ec
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-30 01:18:02 +0900

    :wrench: add firebase dependencies

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import axios from 'axios'
+import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
+import './store'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(VueFire)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
