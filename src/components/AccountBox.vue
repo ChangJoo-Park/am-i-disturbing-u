019439d2349464f6627a7922d6e6d118d2ffbded
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 23:28:38 +0900

    :heavy_plus_sign: add account box

---
+++ src/components/AccountBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <form-box title="Account" :actions="actions">
+    <div class="account-wrapper">
+      <avatar />
+      <div class="account-info">
+        <strong>ChangJoo Park</strong>
+      </div>
+    </div>
+  </form-box>
+</template>
+
+<script>
+import FormBoxLayout from '../layouts/FormBox'
+import Avatar from '../components/Avatar'
+
+export default {
+  components: {
+    'form-box': FormBoxLayout,
+    Avatar
+  },
+  data: function () {
+    return {
+      actions: [
+        {
+          name: 'Edit',
+          action: () => {
+            window.alert('hello')
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.account-wrapper {
+  display: flex;
+  align-items: center;
+}
+.account-info {
+  margin-left: 10px;
+}
+</style>
