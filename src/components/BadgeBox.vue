28422a2c586aaf1f12b2b2a671b64f3f55fa0744
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 22:37:26 +0900

    :lipstick: add wrapper form sidebar box

--- src/components/BadgeBox.vue
+++ src/components/BadgeBox.vue
@@ -1,27 +1,11 @@
 <template>
-  <div class="badge-box">
-    <header class="badge-box-header">
-      <div class="badge-box-title">
-        {{ title }}
-      </div>
-      <div class="badge-box-actions">
-        <button
-          v-for="(action, index) in actions"
-          :key="index"
-          @click="action.onClick"
-          class="badge-box-action"
-        >
-          {{ action.name }}
-        </button>
-      </div>
-    </header>
-    <main class="badge-box-main">
-      <badge-row :badges="badges"/>
-    </main>
-  </div>
+  <form-box :title="title" :actions="actions">
+    <badge-row :badges="badges"/>
+  </form-box>
 </template>
 
 <script>
+import FormBoxLayout from '../layouts/FormBox'
 import BadgeRow from './BadgeRow.vue'
 
 export default {
@@ -43,43 +27,11 @@ export default {
     }
   },
   components: {
+    'form-box': FormBoxLayout,
     BadgeRow
   }
 }
 </script>
 
 <style lang="scss">
-.badge-box {
-  width: 100%;
-  min-width: 250px;
-  min-height: 100px;
-  border: 1px solid black;
-  border-radius: 5px;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-
-  .badge-box-header {
-    border-bottom: 1px solid black;
-    padding: 10px;
-    flex: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  .badge-box-main {
-    flex: 1;
-    padding: 10px;
-  }
-}
-.badge-box-header {
-  .badge-box-title {
-    font-weight: bold;
-  }
-  .badge-box-actions {
-    .badge-box-action:first-child {
-      margin-right: 5px;
-    }
-  }
-}
 </style>
---
+++ src/layouts/FormBox.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="form-box">
+    <header class="form-box-header">
+      <div class="form-box-title">
+        {{ title }}
+      </div>
+      <div class="form-box-actions">
+        <button
+          v-for="(action, index) in actions"
+          :key="index"
+          @click="action.onClick"
+          class="form-box-action"
+        >
+          {{ action.name }}
+        </button>
+      </div>
+    </header>
+    <main class="form-box-main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form-box',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    actions: {
+      type: Array,
+      required: false,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.form-box {
+  width: 100%;
+  min-width: 250px;
+  min-height: 100px;
+  border: 1px solid black;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+
+  .form-box-header {
+    border-bottom: 1px solid black;
+    padding: 10px;
+    flex: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .form-box-main {
+    flex: 1;
+    padding: 10px;
+  }
+}
+.form-box-header {
+  .form-box-title {
+    font-weight: bold;
+  }
+  .form-box-actions {
+    .form-box-action:first-child {
+      margin-right: 5px;
+    }
+  }
+}
+</style>
