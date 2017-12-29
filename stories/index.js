61e5b9c0178e1eec3379389a88a591eee4828fbb
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 16:26:30 +0900

    :heavy_plus_sign:  add layout story

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/vue'
 
 import AvatarStory from './AvatarStory.vue'
 import BadgeStory from './BadgeStory.vue'
+import LayoutStory from './LayoutStory.vue'
 
 storiesOf('Avatar', module)
   .add('기본 사용', () => ({
@@ -13,4 +14,14 @@ storiesOf('Badge', module)
   .add('기본 사용', () => ({
     components: { BadgeStory },
     template: '<badge-story />'
+  }))
+
+storiesOf('Layout', module)
+  .add('Full Layout', () => ({
+    components: { LayoutStory },
+    template: '<layout-story mode="full" />'
+  }))
+  .add('Master Detail Layout', () => ({
+    components: { LayoutStory },
+    template: '<layout-story mode="master-detail" />'
   }))
