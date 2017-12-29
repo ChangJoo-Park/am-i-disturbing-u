cc01e3ea76753593f956bff0272764b9cdef55d8
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 13:16:22 +0900

    :heavy_plus_sign: add storybook - badge

---
+++ stories/BadgeStory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <h1>Text Badge</h1>
+      <text-badge :badge="text"/>
+      <text-badge :badge="secondText"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TextBadge from '../src/components/Badge/TextBadge.vue'
+
+export default {
+  components: {
+    TextBadge
+  },
+  data: function () {
+    return {
+      text: {
+        body: 'Hello World',
+        type: 'text',
+        backgroundColor: '#ddd',
+        color: '#fff'
+      },
+      secondText: {
+        body: 'Hello World',
+        type: 'text',
+        backgroundColor: '#3498db',
+        color: '#000'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
