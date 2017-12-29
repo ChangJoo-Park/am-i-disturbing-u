7316e44bc48575c669f3a159166272dc07018b41
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-29 22:11:27 +0900

    :heavy_plus_sign:  Add methods for badges

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
+    <badge-box title='My Badges' :actions="myBadgeActions"/>
+    <badge-box title='Team Badges' />
   </div>
 </template>
 
 <script>
-export default {
+import BadgeBox from '@/components/BadgeBox'
 
+export default {
+  components: {
+    BadgeBox
+  },
+  data: function () {
+    const myBadgeActions = [
+      {
+        name: 'emoji',
+        onClick: this.openEmojiModal
+      },
+      {
+        name: 'text',
+        onClick: this.openTextModal
+      }
+    ]
+    return {
+      myBadgeActions
+    }
+  },
+  methods: {
+    // Methods for My Badges
+    openEmojiModal: function () {
+      window.alert('Emoji Modal')
+    },
+    openTextModal: function () {
+      window.alert('Text Modal')
+    }
+    // Methods for Team Badge
+  }
 }
 </script>
 
