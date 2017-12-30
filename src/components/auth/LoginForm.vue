95974ea34db371be4cc656aa287d3c95ba89a7e1
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-12-30 20:11:44 +0900

    :heavy_plus_sign: Add login scenarios

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>This is Login Page!</h1>
+    <div>{{ error }}</div>
+    <form @submit.prevent="$emit('on-submit', newUser)" style="display: flex;">
+      <fieldset>
+        <legend>Sign In</legend>
+        <div>
+          <label for="email">Email</label>
+          <input id="email" type="email" autofocus v-model="newUser.email">
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input id="password" type="password" v-model="newUser.password">
+        </div>
+        <div>
+          <input type="submit">
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newUser: {
+        email: '',
+        password: ''
+      },
+      error: null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,76 @@
 <template>
   <div id="login-page">
-    <h1>This is Login Page!</h1>
+    <div v-if="currentUser">
+      {{ currentUser }}
+      <div>
+        <button @click="onClickGoToApp">Go to App</button>
+      </div>
+      <div>
+        <button @click="onClickSignOut">SignOut</button>
+      </div>
+    </div>
+    <login-form v-else @on-submit="onSubmitSignIn" />
+    <button @click="goToSignUp">Sign Up</button>
   </div>
 </template>
 
 <script>
-export default {
+import { mapGetters, mapActions } from 'vuex'
+import { auth } from 'firebase'
+import LoginForm from '@/components/auth/LoginForm'
 
+export default {
+  beforeRouteEnter (to, from, next) {
+    auth().onAuthStateChanged(user => {
+      next(vm => {
+        if (user) {
+          vm.setCurrentUser(user)
+        } else {
+          vm.removeUser()
+        }
+      })
+    })
+  },
+  components: {
+    LoginForm
+  },
+  data () {
+    return {
+      user: null
+    }
+  },
+  computed: {
+    ...mapGetters(['currentUser'])
+  },
+  methods: {
+    ...mapActions(['removeUser', 'setUser']),
+    async onSubmitSignIn (newUser) {
+      try {
+        const { email, password } = newUser
+        await auth().setPersistence(auth.Auth.Persistence.SESSION)
+        const { displayName, isAnnonymous } = await auth().signInWithEmailAndPassword(email, password)
+        this.setCurrentUser({ displayName, email, isAnnonymous })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async setCurrentUser ({ displayName = '', email, isAnnonymous }) {
+      await this.setUser({ displayName, email, isAnnonymous })
+    },
+    onClickGoToApp () {
+      this.$router.push({ name: 'main-page' })
+    },
+    async onClickSignOut () {
+      const isSignOut = window.confirm('로그아웃 하시겠습니까?')
+      if (isSignOut) {
+        await auth().signOut()
+        this.removeUser()
+      }
+    },
+    goToSignUp () {
+      this.$router.push({ name: 'signup-page' })
+    }
+  }
 }
 </script>
 
