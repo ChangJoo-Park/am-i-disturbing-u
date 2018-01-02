e4d6e5901ccecab0bfebdfa430cadb04dbc437bf
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-02 20:37:08 +0900

    :heavy_minus_sign: Remove vuefire, firebase from main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import axios from 'axios'
-import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -10,7 +9,6 @@ import store from './store'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-Vue.use(VueFire)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
