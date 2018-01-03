f8d64bc9fb0eda025c3eea6161e6ec992cb74d9f
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-03 21:57:43 +0900

    :star: :construction:  Add text badge !!

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sidebar-wrapper">
     <div class="sidebar-boxes" v-if="currentUser" >
-      <badge-box title='My Badges' :badges="currentUser.badges"/>
+      <badge-box title='My Badges' :actions="myBadgeActions" :badges="currentUser.badges" />
       <badge-box title='Team Badges' :badges="loadedTeam.badges"/>
       <!-- Status -->
       <status-box :user="currentUser" />
@@ -18,7 +18,7 @@
 import BadgeBox from '@/components/BadgeBox'
 import StatusBox from '@/components/StatusBox'
 import AccountBox from '@/components/AccountBox'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   props: {
@@ -34,7 +34,7 @@ export default {
   computed: {
     ...mapGetters(['currentUser', 'loadedTeam'])
   },
-  data: function () {
+  data () {
     const myBadgeActions = [
       {
         name: 'emoji',
@@ -51,23 +51,48 @@ export default {
       myBadgeActions
     }
   },
+  mounted () {
+    this.$bus.$on('on-badge-clicked', this.onBadgeClicked)
+  },
   methods: {
+    ...mapActions(['removeBadge']),
     // Methods for My Badges
-    openEmojiModal: function () {
+    openEmojiModal () {
       window.alert('Emoji Modal')
     },
-    openTextModal: function () {
-      window.alert('Text Modal')
+    async openTextModal () {
+      const confirmText = window.prompt('Badge 텍스트를 입력하세요')
+      if (confirmText) {
+        const Cookie = require('js-cookie')
+
+        const result = await this.$http({
+          method: 'POST',
+          url: '/api/badges',
+          headers: {authorization: Cookie.get('token')},
+          data: {
+            type: 'text',
+            owner: 'member',
+            body: confirmText
+          }
+        })
+        console.log('result => ', result)
+      }
     },
     // Methods for Team Badge
     // SignOut
     async onClickSignOut () {
       const isSignOut = window.confirm('로그아웃 하시겠습니까?')
       if (isSignOut) {
-        this.$router.replace({ name: 'login-page' })
+        this.$router.replace({name: 'login-page'})
       }
     },
-    async onNewBadgeCreated () {
+    async onNewBadgeCreated () {},
+    async onBadgeClicked (badge) {
+      const isMine = badge.creator === this.currentUser._id && badge.owner === 'member'
+      if (isMine) {
+        console.log('삭제할 수 있음')
+        await this.removeBadge(badge)
+      }
     }
   }
 }
