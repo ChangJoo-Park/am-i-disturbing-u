055e7ea43c43c892aa784f30d4b00e748aef5a3d
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-06 11:51:26 +0900

    :heavy_plus_sign: Add material design icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,54 @@
+<template>
+  <i class="material-icons" :class="iconClass" :style="iconStyle">{{ name }}</i>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: 'normal',
+      required: false,
+      validator (value) {
+        return ['small', 'normal', 'large'].includes(value)
+      }
+    },
+    color: {
+      type: String,
+      default: '#fff'
+    },
+    backgroundColor: {
+      type: String,
+      default: '#333'
+    }
+  },
+  computed: {
+    iconClass () {
+      return `material-icons-${this.size}`
+    },
+    iconStyle () {
+      return { backgroundColor: this.backgroundColor, color: this.color }
+    }
+  }
+}
+</script>
+
+<style>
+.material-icons {
+  border-radius: 100%;
+}
+.material-icons-normal {
+  font-size: 15px;
+}
+
+.material-icons-small {
+  font-size: 13px;
+}
+
+.material-icons-large {
+  font-size: 18px;
+}
+</style>
